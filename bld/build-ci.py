46dac32af1816932fcf0c12bc223f853c75dd2e3
Afonso França <afonso.franca@gmail.com>
2012-08-07 11:30:51 -0300

    Mais ajustes no Build

--- bld/build-ci.py
+++ bld/build-ci.py
@@ -26,20 +26,21 @@ pubDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../pub' ) 
 scriptDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/WebUtils/Scripts/WebUtils' ) )
 tempDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../temp' ) )
 resDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../res' ) )
+repoUrl = 'http://cronos:9090/gasrd/Web/pub/Dongle.Net/trunk'
 
 #--------------------------------------------------------------------
-bp = Builder( "WebUtils" )
+bp = Builder( "Dongle.Net" )
 bp.addStep( IncrementBuildVersionStep( assemblyPath, projectRootDir ) )
 bp.addStep( MainBuild() )
 
-bp.addStep( SvnDeleteStep('http://cronos:9090/gasrd/Web/pub/WebUtils/trunk'))
+bp.addStep( SvnDeleteStep(repoUrl))
 bp.addStep( DelTreeStep( pubDir ) )
 bp.addStep( SvnUpdateDirStep( projectRootDir ) )
 
 bp.addStep( DelTreeStep( tempDir + '\\WebUtils\\testresults' ) )  
-bp.addStep( CopyFilteredFilesStep( WebUtilsFileFilter(), tempDir + "\\WebUtils", pubDir + "//Bin" ) )
+bp.addStep( CopyFilteredFilesStep( WebUtilsFileFilter(), tempDir + "\\WebUtils", pubDir ) )
 bp.addStep( DelTreeStep( tempDir ) ) 
-bp.addStep( SvnImportDirStep( pubDir, 'http://cronos:9090/gasrd/Web/pub/WebUtils/trunk' ) )
+bp.addStep( SvnImportDirStep( pubDir, repoUrl ) )
 
 if not bp.build():
     sys.exit(1)
--- bld/build-deploy-revision.py
+++ bld/build-deploy-revision.py
@@ -24,6 +24,7 @@ assemblyDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../sr
 pubDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../pub' ) )
 scriptDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/WebUtils/Scripts/WebUtils' ) )
 tempDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../temp' ) )
+repoUrl = 'http://cronos:9090/gasrd/Web/pub/Dongle.Net/trunk'
 
 #--------------------------------------------------------------------
 bp = Builder( "Gbwcd" )
@@ -31,15 +32,15 @@ bp.addStep( IncrementRevisionVersionStep( assemblyPath, projectRootDir ) )
 
 bp.addStep( MainBuild() )
 
-bp.addStep( SvnDeleteStep('http://cronos:9090/gasrd/Web/pub/WebUtils/trunk'))
+bp.addStep( SvnDeleteStep(repoUrl))
 bp.addStep( DelTreeStep( pubDir ) )
 bp.addStep( SvnUpdateDirStep( projectRootDir ))
 
 bp.addStep( DelTreeStep( tempDir + '\\WebUtils\\testresults' ) )  
-bp.addStep( CopyFilteredFilesStep( WebUtilsFileFilter(), tempDir + "\\WebUtils", pubDir + "//Bin" ) )
+bp.addStep( CopyFilteredFilesStep( WebUtilsFileFilter(), tempDir + "\\WebUtils", pubDir ) )
 bp.addStep( CopyDirStep( scriptDir, pubDir + '//Scripts' ) )
 bp.addStep( DelTreeStep( tempDir ) ) 
-bp.addStep( SvnImportDirStep( pubDir, 'http://cronos:9090/gasrd/Web/pub/WebUtils/trunk' ) )
+bp.addStep( SvnImportDirStep( pubDir, repoUrl ) )
 bp.addStep( SvnCommitDirStep( assemblyDir, 1  ) )
 bp.addStep( SvnCreateTagDirStep( pubDir,  'http://cronos:9090/gasrd/Web/pub/WebUtils/tags', assemblyPath ) )
 
