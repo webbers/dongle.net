bc1a2a8f1843683570c236b55061f76f03643864
Afonso França <afonso.franca@gmail.com>
2012-09-06 09:55:44 -0300

    Ajustes nos Builds de Deploy
    Minor e Revision

--- bld/build-deploy-revision.py
+++ bld/build-deploy-revision.py
@@ -20,28 +20,34 @@ class DongleFileFilter(FileFilter):
         FileFilter.__init__( self, ['*\\.svn\\*', '\\uploads' ], ['*Test*', '*Compiler*', 'Ploe*', 'Moq*', 'System*', 'mscorlib*', '*esults*' ], ['.pdb','.nlp']  )
 
 projectRootDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../' ) )
-assemblyPath = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/Dongle.Web/Properties/AssemblyInfo.cs' ) )
-assemblyDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/Dongle.Web/Properties/' ) )
+assemblyPath1 = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/Dongle/Properties/AssemblyInfo.cs' ) )
+assemblyPath2 = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/Dongle.Web/Properties/AssemblyInfo.cs' ) )
+
+assemblyDir1 = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/Dongle/Properties/' ) )
+assemblyDir2 = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../src/Dongle.Web/Properties/' ) )
+
 pubDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../pub' ) )
 tempDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../temp' ) )
 repoUrl = 'http://cronos:9090/gasrd/Web/pub/Dongle.Net/trunk'
 
 #--------------------------------------------------------------------
 bp = Builder( "Dongle.Net" )
-bp.addStep( IncrementRevisionVersionStep( assemblyPath, projectRootDir ) )
-
+bp.addStep( IncrementRevisionVersionStep( assemblyPath1, projectRootDir ) )
+bp.addStep( IncrementRevisionVersionStep( assemblyPath2, projectRootDir ) )
 bp.addStep( MainBuild() )
 
-bp.addStep( SvnDeleteStep(repoUrl))
 bp.addStep( DelTreeStep( pubDir ) )
-bp.addStep( SvnUpdateDirStep( projectRootDir ))
+bp.addStep( DelTreeStep( tempDir + '\\Dongle\\testresults' ) )  
 
-bp.addStep( DelTreeStep( tempDir + '\\Dongle.Web\\testresults' ) )  
 bp.addStep( CopyFilteredFilesStep( DongleFileFilter(), tempDir + "\\Dongle.Web", pubDir ) )
+bp.addStep( CopyFilteredFilesStep( DongleFileFilter(), tempDir + "\\Dongle", pubDir ) )
 bp.addStep( DelTreeStep( tempDir ) ) 
+
+bp.addStep( SvnDeleteStep(repoUrl))
 bp.addStep( SvnImportDirStep( pubDir, repoUrl ) )
-bp.addStep( SvnCommitDirStep( assemblyDir, 1, sys.argv[1], sys.argv[2] ) )
 bp.addStep( SvnCreateTagDirStep( repoUrl,  'http://cronos:9090/gasrd/Web/pub/Dongle.Net/tags', assemblyPath ) )
+
+bp.addStep( SvnCommitDirStep( assemblyDir, 1, sys.argv[1], sys.argv[2]  ) )
 
 if not bp.build():
     sys.exit(1)
