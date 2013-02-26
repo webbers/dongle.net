50b4effb9c806c36204ad20d467ef0a6e923fd14
Walter Amorim <amorimwalter@gmail.com>
2013-02-26 14:35:08 -0300

    Nova função Distribute nas extensões do IEnumerable

---
+++ src/Dongle.Tests/System/EnumerableExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Dongle.System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dongle.Tests.System
+{
+    [TestClass]
+    public class EnumerableExtensionsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var values = new [] {10d, 70d, 1d}.Distribute(100);
+            Assert.AreEqual(100, values.Sum());
+
+            values = new[] { 1d, 1d, 1d }.Distribute(100);
+            Assert.AreEqual(100, values.Sum());
+
+            values = new[] { 10d }.Distribute(100);
+            Assert.AreEqual(100, values.Sum());
+
+            values = new[] { 100d, 200d }.Distribute(100);
+            Assert.AreEqual(100, values.Sum());
+        }
+    }
+}
--- src/Dongle/System/EnumerableExtensions.cs
+++ src/Dongle/System/EnumerableExtensions.cs
@@ -37,5 +37,49 @@ namespace Dongle.System
             }
             return count + " " + itemsWord;
         }
+
+        public static IEnumerable<int> Distribute(this IEnumerable<double> weights, int amount)
+        {
+            var weightsList = weights.ToList();
+            var totalWeight = weightsList.Sum();
+            var length = weightsList.Count();
+
+            var actual = new double[length];
+            var error = new double[length];
+            var rounded = new int[length];
+
+            var added = 0;
+
+            var i = 0;
+            foreach (var w in weightsList)
+            {
+                actual[i] = amount * (w / totalWeight);
+                rounded[i] = (int)Math.Floor(actual[i]);
+                error[i] = actual[i] - rounded[i];
+                added += rounded[i];
+                i += 1;
+            }
+
+            while (added < amount)
+            {
+                var maxError = 0.0;
+                var maxErrorIndex = -1;
+                for (var e = 0; e < length; ++e)
+                {
+                    if (error[e] > maxError)
+                    {
+                        maxError = error[e];
+                        maxErrorIndex = e;
+                    }
+                }
+
+                rounded[maxErrorIndex] += 1;
+                error[maxErrorIndex] -= 1;
+
+                added += 1;
+            }
+
+            return rounded;
+        }
     }
 }
