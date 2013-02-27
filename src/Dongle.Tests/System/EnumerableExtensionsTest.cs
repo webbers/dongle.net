a03ce6bb7736b59f8b6ae96894b41ac46c10cf41
Walter Amorim <amorimwalter@gmail.com>
2013-02-27 10:21:46 -0300

    Correção no teste

--- src/Dongle.Tests/System/EnumerableExtensionsTest.cs
+++ src/Dongle.Tests/System/EnumerableExtensionsTest.cs
@@ -11,7 +11,7 @@ namespace Dongle.Tests.System
     public class EnumerableExtensionsTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDistribute()
         {
             var values = new [] {10d, 70d, 1d}.Distribute(100);
             Assert.AreEqual(100, values.Sum());
@@ -23,9 +23,6 @@ namespace Dongle.Tests.System
             Assert.AreEqual(100, values.Sum());
 
             values = new[] { 100d, 200d }.Distribute(100);
-            Assert.AreEqual(100, values.Sum());
-
-            values = new[] { 0d, 0d }.Distribute(100);
             Assert.AreEqual(100, values.Sum());
         }
     }
