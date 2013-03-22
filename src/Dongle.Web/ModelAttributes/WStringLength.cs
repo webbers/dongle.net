848f199553ea92f08d2675ab1d4aa3b666b0584b
Renan Borges <renanborges02@gmail.com>
2013-03-22 15:27:41 -0300

    wstringlength

--- src/Dongle.Web/ModelAttributes/WStringLength.cs
+++ src/Dongle.Web/ModelAttributes/WStringLength.cs
@@ -6,35 +6,37 @@ using Dongle.Web.Resources;
 
 namespace Dongle.Web.ModelAttributes
 {
-    public class WStringLength : StringLengthAttribute, IClientValidatable
+    public class WStringLength : ValidationAttribute, IClientValidatable
     {
-        private readonly int _maxlength;
+        private readonly StringLengthAttribute _stringLengthAttribute;
+        private readonly int _maximumLength;
 
         public WStringLength(int maximumLength)
-            : base(maximumLength)
         {
-            _maxlength = maximumLength;
+            _maximumLength = maximumLength;
+            _stringLengthAttribute = new StringLengthAttribute(maximumLength);
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if (Convert.ToInt32(value) <= _maxlength)
+            var msg = String.Format(DongleResource.InvalidStringLength, _maximumLength);
+
+            if (!_stringLengthAttribute.IsValid(value))
             {
-                return null;
+                return new ValidationResult(msg, new[] { validationContext.MemberName });
             }
-            return new ValidationResult(FormatErrorMessage(ErrorMessage));
+            return ValidationResult.Success;
         }
 
         public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
         {
-            var msg = ErrorMessage ?? DongleResource.InvalidStringLength;
+            var msg = String.Format(DongleResource.InvalidStringLength, _maximumLength);
 
             var rule = new ModelClientValidationRule
             {
                 ErrorMessage = msg,
-                ValidationType = "wstringlength",
+                ValidationType = "stringlength",
             };
-            rule.ValidationParameters.Add("maxlength", _maxlength);
             yield return rule;
         }
     }
