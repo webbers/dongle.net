0e4830709bf853482eaea9f6790a064b216f3d28
Luiz Fernando de Moura <dede_moura@hotmail.com>
2013-05-13 09:27:41 -0300

    Remoção de comentários

--- src/Dongle/System/Networking/NamedPipes/StringStream.cs
+++ src/Dongle/System/Networking/NamedPipes/StringStream.cs
@@ -34,32 +34,6 @@ namespace Dongle.System.Networking.NamedPipes
             }
         }
 
-        //public string ReadString()
-        //{
-        //    var lenBytes = new byte[20];
-        //    _ioStream.Read(lenBytes, 0, 20);
-        //    var len = Convert.ToInt32(_streamEncoding.GetString(lenBytes));            
-        //    if (len <= 0)
-        //        return "";
-        //    var inBuffer = new byte[len];
-        //    _ioStream.Read(inBuffer, 0, len);
-        //    return _streamEncoding.GetString(inBuffer);
-        //}
-
-        //public int WriteString(string outString)
-        //{
-        //    var outBuffer = _streamEncoding.GetBytes(outString);
-        //    var len = outBuffer.Length;
-        //    var lenStr = outBuffer.Length.ToString().PadLeft(10, '0');
-        //    var lenBuffer = _streamEncoding.GetBytes(lenStr);
-        //    _ioStream.Write(lenBuffer, 0, lenBuffer.Length);
-        //    _ioStream.Flush();
-        //    _ioStream.Write(outBuffer, 0, len);
-        //    _ioStream.Flush();
-        //    return outBuffer.Length + 2;
-        //}
-
-
         public string ReadStringAsync(int timeout = 2000)
         {
             var readTask = Task.Factory.StartNew(() => ReadString());
