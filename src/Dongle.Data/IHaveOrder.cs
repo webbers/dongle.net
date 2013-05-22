cc94c98d4da7565c472b5236658e31551bd3bcf9
afonso.oliveira <afonso.franca@gmail.com>
2013-05-22 16:29:24 -0300

    Added IHaveOrder to Dongle.Data

---
+++ src/Dongle.Data/IHaveOrder.cs
@@ -0,0 +1,13 @@
+﻿namespace Dongle.Data
+{
+    /// <summary>
+    /// Sinaliza tabelas cujo registro possuem uma ordem. Pode ser usado para inserir botões de ordenação nos itens da lista.
+    /// </summary>
+    public interface IHaveOrder
+    {
+        /// <summary>
+        /// Ordem
+        /// </summary>
+        long Order { get; set; }
+    }
+}
