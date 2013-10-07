7a6087de034b6fb799d92d372eba2e778e640c4a
Rodrigo Aiala Feijolo Rodrigues <r.feijolo@gmail.com>
2013-10-07 11:32:06 -0300

    adicionando EnumExtensions.cs

---
+++ src/Dongle.Web/Extensions/EnumExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Web.Mvc;
+
+namespace Dongle.Web.Extensions
+{
+    public static class EnumExtensions
+    {
+        private static ResourceManager _resourceManager;
+
+        public static IEnumerable<SelectListItem> ToSelectList(this Type enumType, Func<SelectListItem, bool> filter = null)
+        {
+            var elementsValue = (long[])enumType.GetEnumValues();
+
+            var listItems = elementsValue.Select(element => new SelectListItem
+            {
+                Selected = false,
+                Value = Convert.ToString(element, CultureInfo.InvariantCulture),
+                Text = GetIdNameResource(element, enumType)
+            });
+            return listItems;
+        }
+
+        public static string GetIdNameResource(long id, Type enumType)
+        {
+            var name = enumType.GetEnumName(id);
+            if (name == null)
+            {
+                return id.ToString(CultureInfo.InvariantCulture);
+            }
+            if (_resourceManager != null)
+            {
+                return _resourceManager.GetString(name) ?? name;
+            }
+            return name;
+        }
+
+        public static void SetResourceManager(ResourceManager resourceManager)
+        {
+            _resourceManager = resourceManager;
+        }
+    }
+}
