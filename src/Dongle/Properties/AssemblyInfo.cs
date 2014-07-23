80e111e01a535f2d8dcb759dc23bb56d82972020
buildwebbers <webbersteam@gmail.com>
2014-07-23 20:04:40 +0000

    Commited by Build

--- src/Dongle/Properties/AssemblyInfo.cs
+++ src/Dongle/Properties/AssemblyInfo.cs
@@ -33,9 +33,10 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.19.74.0")]
-[assembly: AssemblyFileVersion("0.19.74.0")]
+[assembly: AssemblyVersion("0.19.75.0")]
+[assembly: AssemblyFileVersion("0.19.75.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
+
 
 
 
