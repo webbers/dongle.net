a23f914567e58a124fd19413d21eb7521d6e9899
gilmarsquinelato <gilmarsquinelato@gmail.com>
2015-07-15 09:30:20 -0300

    Updated to .NET Framework 4.5

--- src/Dongle.Web/Properties/AssemblyInfo.cs
+++ src/Dongle.Web/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.30.4.0")]
-[assembly: AssemblyVersion("0.30.4.0")]
-[assembly: AssemblyFileVersion("0.30.4.0")]
+// [assembly: AssemblyVersion("0.30.8.0")]
+[assembly: AssemblyVersion("0.30.8.0")]
+[assembly: AssemblyFileVersion("0.30.8.0")]
 
 
 
